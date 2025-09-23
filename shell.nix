f020348e36d25cec52841350b10a7314dac8742e
zapashcanon <leo@ndrs.fr>
2025-09-23 11:45:33 +0200

    add a `shell.nix`

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  }) {}
+}:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.ocamlPackages; [
+    dune_3
+    findlib
+    js_of_ocaml-compiler
+    ocaml
+    ocamlformat
+    odoc
+  ];
+  buildInputs = with pkgs.ocamlPackages; [
+    ppxlib
+  ];
+}
